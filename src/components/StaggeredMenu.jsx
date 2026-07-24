55c54cbf08b923b2a17247650b6801d83fc8eb5a
Siddharth Sharma <siddharth.sharma0337@gmail.com>
2026-07-24 23:37:16 +0530

    ok

--- src/components/StaggeredMenu.jsx
+++ src/components/StaggeredMenu.jsx
@@ -119,7 +119,15 @@ export default function StaggeredMenu({ isOpen, onToggle }) {
        <ul className="menu-nav">
          {menuItems.map((item, i) => (
            <li key={item.label} ref={(el) => { navItemsRef.current[i] = el; }}>
-             <a href={item.href} className="no-magnetic" onClick={(e) => e.preventDefault()}>{item.label}</a>
+             <a 
+               href={item.href} 
+               className="no-magnetic" 
+               onClick={(e) => {
+                 if (item.href !== '/') e.preventDefault();
+               }}
+             >
+               {item.label}
+             </a>
            </li>
          ))}
        </ul>
